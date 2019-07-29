93f70591fa27889110742d9d11c24292a73ca19c
Wulfram <malik.ru@hotmail.com>
2019-07-29 18:53:18 +0300

    upd filter.py

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import time
+import cython
 
 # К1_20181207_110935.csv
 
@@ -8,22 +9,22 @@ def filter(path):
     file = open(path, 'r')
     data = pd.read_csv(file, sep=';', names=['time', 'part', 'x', 'y', 'z'])
 
-    parts = set(data['part'])
+    parts = list(set(data['part']))
     times = sorted(set(data['time']))
     p_col = len(parts)
-    # result = [[0.0, 0.0, 0.0, 0.0, 0.0,] for i in range(20)]
-    result = []
+    result = [[0.0, 0.0, 0.0, 0.0, 0.0,] for i in range(20)]
+    # result = []
 
     for t in times:
         for p in parts:
-            if not data[(data['time'] == t) & (data['part'] == p)].empty:
-                # print(f"{data[(data['time'] == t) & (data['part'] == p)]}\n-----------------------", file=open('logi.log', 'a'))
-                values = data[(data['time'] == t) & (data['part'] == p)].values[0]
-                result.append([t, p, values[2], values[3], values[4]])
-            elif len(result) > p_col:
+            row = data[(data['time'] == t) & (data['part'] == p)]
+            if not row.empty:
+                # print(f"{data[(data['time'] == t) & (data['part'] == p)]}\n-----------------------")
+                values = row.values[0]
+                result.append([values[0], values[1], values[2], values[3], values[4]])
+            else:
                 # print(f"{t}|{p}|{result[-p_col]}", file=open('logi.log', 'a'))
                 result.append([t, p, result[-p_col][2], result[-p_col][3], result[-p_col][4]])
-            else:
-                result.append([t, p, 0.0, 0.0, 0.0])
+    result = result[p_col:]
     print(time.time() - start_time)
     return pd.DataFrame(result, columns=['time', 'part', 'x', 'y', 'z'])
